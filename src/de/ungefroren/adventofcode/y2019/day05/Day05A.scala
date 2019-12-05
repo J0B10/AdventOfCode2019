6e7bd9b305ba1b1137e8c76a86abc06acb82fc7e
Jonas <joblo.2213@gmail.com>
2019-12-05 19:02:24 +0100

    Finiis Day05A

---
+++ src/de/ungefroren/adventofcode/y2019/day05/Day05A.scala
@@ -0,0 +1,47 @@
+package de.ungefroren.adventofcode.y2019.day05
+
+import scala.annotation.tailrec
+import scala.io.{Source, StdIn}
+
+object Day05A {
+
+  val input = Source.fromInputStream(getClass.getResourceAsStream("puzzle_input.txt"))
+
+  @tailrec
+  def runIntcode(pos: Int, intcode: Array[Int]): Array[Int] = {
+    val opcode = intcode(pos)
+    def param(index: Int): Int = {
+      val mode = (opcode / math.pow(10, index+1).toInt) % 10
+      if (mode == 1) {
+        val i = pos+index
+        i
+      } else {
+        val i = intcode(pos+index)
+        i
+      }
+    }
+    (opcode % 100) match {
+      case 1 =>
+        intcode(param(3)) = intcode(param(1)) + intcode(param(2))
+        runIntcode(pos + 4, intcode)
+      case 2 =>
+        intcode(param(3)) = intcode(param(1))* intcode(param(2))
+        runIntcode(pos + 4, intcode)
+      case 3 =>
+        print("> ")
+        intcode(param(1)) = StdIn.readInt()
+        runIntcode(pos + 2, intcode)
+      case 4 =>
+        println(s"[TEST]: ${intcode(param(1))}")
+        runIntcode(pos + 2, intcode)
+      case 99 => intcode
+      case _ => throw new Exception(s"Invalid opcode: $opcode")
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val intcode = input.mkString.split(",").map(_.replaceAll("\\s", "")).map(_.toInt)
+    runIntcode(0, intcode)
+  }
+
+}
