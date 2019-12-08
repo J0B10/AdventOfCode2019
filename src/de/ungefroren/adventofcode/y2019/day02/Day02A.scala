bd24635ed72c9d33f2a592a748eafee96713384b
Jonas <joblo.2213@gmail.com>
2019-12-08 13:19:26 +0100

    Rework day 02 to use new intcode computer

--- src/de/ungefroren/adventofcode/y2019/day02/Day02A.scala
+++ src/de/ungefroren/adventofcode/y2019/day02/Day02A.scala
@@ -1,35 +1,22 @@
 package de.ungefroren.adventofcode.y2019.day02
 
-import scala.annotation.tailrec
+import de.ungefroren.adventofcode.y2019.intcodeComputer.IntcodeComputer
+import de.ungefroren.adventofcode.y2019.intcodeComputer.instructions.defaults
+
 import scala.io.Source
 
 object Day02A {
 
   val input = Source.fromInputStream(getClass.getResourceAsStream("puzzle_input.txt"))
 
-  @tailrec
-  def runIntcode(pos: Int, intcode: Array[Int]): Array[Int] = {
-    val opcode = intcode(pos)
-    val pos1 = intcode(pos+1)
-    val pos2 = intcode(pos+2)
-    val resPos = intcode(pos+3)
-    opcode match {
-      case 1 =>
-        intcode(resPos) = intcode(pos1) + intcode(pos2)
-        runIntcode(pos + 4, intcode)
-      case 2 =>
-        intcode(resPos) = intcode(pos1) * intcode(pos2)
-        runIntcode(pos + 4, intcode)
-      case 99 => intcode
-      case _ => throw new Exception(s"Invalid opcode: $opcode")
-    }
-  }
 
   def main(args: Array[String]): Unit = {
     val intcode = input.mkString.split(",").map(_.replaceAll("\\s", "")).map(_.toInt)
-    intcode(1) = 12
-    intcode(2) = 2
-    val result = runIntcode(0,intcode)(0)
+    val computer = new IntcodeComputer(intcode)
+    computer.memory(1) = 12
+    computer.memory(2) = 2
+    computer.run()
+    val result = computer.memory(0)
     print(s"Value at position 0 is: $result")
   }
 
--- src/de/ungefroren/adventofcode/y2019/day02/Day02B.scala
+++ src/de/ungefroren/adventofcode/y2019/day02/Day02B.scala
@@ -1,6 +1,8 @@
 package de.ungefroren.adventofcode.y2019.day02
 
-import scala.annotation.tailrec
+import de.ungefroren.adventofcode.y2019.intcodeComputer.IntcodeComputer
+import de.ungefroren.adventofcode.y2019.intcodeComputer.instructions.defaults
+
 import scala.io.Source
 
 object Day02B {
@@ -9,32 +11,15 @@ object Day02B {
 
   val targetOutput = 19690720
 
-  @tailrec
-  def runIntcode(pos: Int, intcode: Array[Int]): Array[Int] = {
-    val opcode = intcode(pos)
-    val pos1 = intcode(pos+1)
-    val pos2 = intcode(pos+2)
-    val resPos = intcode(pos+3)
-    opcode match {
-      case 1 =>
-        intcode(resPos) = intcode(pos1) + intcode(pos2)
-        runIntcode(pos + 4, intcode)
-      case 2 =>
-        intcode(resPos) = intcode(pos1) * intcode(pos2)
-        runIntcode(pos + 4, intcode)
-      case 99 => intcode
-      case _ => throw new Exception(s"Invalid opcode: $opcode")
-    }
-  }
-
   def main(args: Array[String]): Unit = {
     val intcode = input.mkString.split(",").map(_.replaceAll("\\s", "")).map(_.toInt)
     for (noun <- 0 to 99) {
       for (verb <- 0 to 99) {
-        val memory = intcode.clone
-        memory(1) = noun
-        memory(2) = verb
-        val result = runIntcode(0, memory)(0)
+        val computer = new IntcodeComputer(intcode.clone)
+        computer.memory(1) = noun
+        computer.memory(2) = verb
+        computer.run()
+        val result = computer.memory(0)
         if (result == targetOutput) {
           print(s"Traget output of $targetOutput was generated using noun=$noun and verb=$verb. Result: ${100*noun+verb}")
           return
